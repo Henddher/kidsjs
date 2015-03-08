38b086f3dbb3485228f9786e949725f6570f15f7
juanpablo <henddher.pedroza@gmail.com>
2015-03-08 16:52:34 -0500

    simon says rects

--- main.js
+++ main.js
@@ -4,8 +4,26 @@ function main() {
     var context = canvas.getContext("2d");
 
     context.font = "30px Arial";
-    context.fillStyle = "royalblue";
+    context.fillStyle = "red";
+    
 
-    context.fillText("Hello World", 50, 50);
-    //context.fillRect(10, 10, 300, 300);
+
+    //context.fillText("Juan Pablo Is Super Awesome", 50, 300);
+    context.fillRect(30, 30, 100, 100);
+
+ context.fillStyle = "blue";
+ context.fillRect(130, 30, 100, 100);
+ context.fillStyle = "gold";
+ context.fillRect(30, 130, 100, 100);
+context.fillStyle = "green";
+ context.fillRect(130, 130, 100, 100);
+    
+
+    
+        
+
+
+
+	 // context.font = "20px Arial";
+	//context.fillText("cheese", 50, 400);
 }
