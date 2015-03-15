ba5ce62d794c78a79876aa56d5a317f7fc1de6ec
Henddher  Pedroza <henddher.pedroza@gmail.com>
2015-03-15 15:24:58 -0500

    fill rect and text

--- main.js
+++ main.js
@@ -3,9 +3,24 @@ function main() {
     var canvas = document.getElementById("canvas");
     var context = canvas.getContext("2d");
 
-    context.font = "30px Arial";
-    context.fillStyle = "royalblue";
+    context.font = "100px Cursive";
+    context.fillStyle = "green";
 
-    context.fillText("Hello World", 50, 50);
-    //context.fillRect(10, 10, 300, 300);
+    // a rect
+    context.fillRect(100, 200, 200, 200);
+    context.fillStyle = "blue";
+    context.fillRect(300, 400, 200, 200);
+    context.fillStyle = "red";
+    context.fillRect(100, 400, 200, 200);
+    context.fillStyle = "yellow";
+    context.fillRect(300, 200, 200, 200);
+    context.fillStyle = "blue";
+    
+    // text on the screen
+     context.fillText("I love dogs", 0, 100);
+     context.fillStyle = "purple";
+     context.font = "50px Cursive";
+     context.fillText("Ailani Pedroza", 0, 650);
 }
+
+
